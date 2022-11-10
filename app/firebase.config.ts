5b5e49e44fb279b5acaab599b799c24cf993478e
Roby Huzwandar <roby.huzwandar@flash-coffee.com>
2022-11-10 22:43:06 +0800

    update git ignore

--- app/firebase.config.ts
+++ app/firebase.config.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth} from "firebase/auth";
+import { getAuth, inMemoryPersistence, setPersistence } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,7 +8,7 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // your config
+
 };
 
 // Initialize Firebase
