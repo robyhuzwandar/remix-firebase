5de120c5d45c9c0bee1e11eb28a39ef1279f4b5a
Roby Huzwandar <roby.huzwandar@flash-coffee.com>
2022-11-10 22:42:05 +0800

    remove config

--- app/firebase.config.ts
+++ app/firebase.config.ts
@@ -8,14 +8,7 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCFd6J7q0UxYd0CnEsuiL6Y8j_-lVWyMg4",
-  authDomain: "remix-todo-b54a7.firebaseapp.com",
-  databaseURL: "https://remix-todo-b54a7-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "remix-todo-b54a7",
-  storageBucket: "remix-todo-b54a7.appspot.com",
-  messagingSenderId: "547894053845",
-  appId: "1:547894053845:web:7549ec0129cba0daf9cb5c",
-  measurementId: "G-KM096PNLGG"
+  // your config
 };
 
 // Initialize Firebase
